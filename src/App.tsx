f6f68bcc2f17c3ef80484dba32f066b96a1f98cf
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-10-05 08:11:17 +0530

    Interface

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,20 @@ function App() {
   isStudent = true;
   hoobies = ["Cricket", "MMA", "Coding"]
   role = ["Software Developer", 3]
+
+  //making of interface for object
+
+  type PersonInterface = {
+    //creatign interface for objects and the types
+    name:string,
+    age:number
+  }
+  let person: PersonInterface ={
+    name: "Sachin",
+    age: 23
+  }
+
+  console.log(person)
   
   return (
    <div>
