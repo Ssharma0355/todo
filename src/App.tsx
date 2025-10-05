80be4bd996e0b3c8ff4bb2b3ec474020166f07ba
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-10-05 08:02:28 +0530

    learning types in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  let name : string;
+  let age: number;
+  let isStudent: boolean;
+  let hoobies: string[];
+  let role:[string,number]; // tuple in typescript
+
+  name = "Sachin";
+  age = 23;
+  isStudent = true;
+  hoobies = ["Cricket", "MMA", "Coding"]
+  role = ["Software Developer", 3]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+
+   </div>
   );
 }
 
