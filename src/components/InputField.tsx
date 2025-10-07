1b925e7d47e2a316cf18aa1f5bbb62cf34d9e70c
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-10-07 09:45:05 +0530

    set

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,10 +4,11 @@ interface Props{
     todo:string;
     SetTodo:React.Dispatch<React.SetStateAction<string>>
 }
-const InputField = ({todo, SetTodo}:Props) => {
+const InputField: React.FC<Props> = ({todo, SetTodo}) => {
+    console.log(todo)
   return (
     <form className='input'>
-        <input type="text" placeholder='Enter your task' className='input_box' />
+        <input type="text" value={todo} onChange={(e)=>SetTodo(e.target.value)} placeholder='Enter your task' className='input_box' />
         <button className='button' type='submit'>Go</button>
     </form>
   )
